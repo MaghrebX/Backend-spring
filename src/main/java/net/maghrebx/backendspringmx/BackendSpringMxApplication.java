f7b8484750243354edcc7f1fcc9759622b778666
samir5636 <zianisamir5636789@gmail.com>
2024-12-09 15:16:00 +0100

    Initial commit for Spring Boot project

---
+++ src/main/java/net/maghrebx/backendspringmx/BackendSpringMxApplication.java
@@ -0,0 +1,13 @@
+package net.maghrebx.backendspringmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringMxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringMxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/maghrebx/backendspringmx/BackendSpringMxApplicationTests.java
@@ -0,0 +1,13 @@
+package net.maghrebx.backendspringmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringMxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
